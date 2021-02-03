2d9bd9e25a4bcfdcccace7732849bf80ea12a661
kusa-mochi <whoatemyapplepie@gmail.com>
2021-02-03 22:47:55 +0900

    ComboBoxの選択リセットのサンプルを追加した。

--- PracticeEntiryFramework/ViewModels/MainWindowContentViewModel.cs
+++ PracticeEntiryFramework/ViewModels/MainWindowContentViewModel.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace PracticeEntiryFramework.ViewModels
 {
     public class MainWindowContentViewModel : BindableBase
     {
+        public MainWindowContentViewModel()
+        {
+            ComboBoxItems = new ObservableCollection<long> { 100L, 200L, 300L };
+        }
+
+        private int _ComboBoxSelectedIndex = -1;
+        public int ComboBoxSelectedIndex
+        {
+            get { return _ComboBoxSelectedIndex; }
+            set { SetProperty(ref _ComboBoxSelectedIndex, value); }
+        }
+
+        private ObservableCollection<long> _ComboBoxItems = null;
+        public ObservableCollection<long> ComboBoxItems
+        {
+            get { return _ComboBoxItems; }
+            set { SetProperty(ref _ComboBoxItems, value); }
+        }
+
+        private DelegateCommand _ResetCommand   ;
+        public DelegateCommand ResetCommand =>
+            _ResetCommand ?? (_ResetCommand = new DelegateCommand(ExecuteResetCommand));
+
+        void ExecuteResetCommand()
+        {
+            ComboBoxSelectedIndex = -1;
+        }
     }
 }
